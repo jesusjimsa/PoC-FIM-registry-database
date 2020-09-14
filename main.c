8e7df6de9065ae63938d0e8c2a62284bcb7c41ca
jesusjimsa <jesusjimsa@icloud.com>
2020-09-14 09:40:43 +0100

    Update fill and print functions

--- main.c
+++ main.c
@@ -14,9 +14,16 @@
 #include <openssl/evp.h>
 #include <openssl/sha.h>
 
-#define TEST_PATH_START "/root/tiempos.csv"
-#define TEST_PATH_END "/home/user/test/file_4"
-#define PATH_MAX 4096
+#define DEF_REG_NAME "reg_name_"
+#define DEF_REG_PATH "HKEY_LOCAL_MACHINE\\TEST"
+#define DEF_PERM "-rwxrwxrwx"
+#define DEF_UID "0"
+#define DEF_GID "0"
+#define DEF_USER_NAME "root"
+#define DEF_GROUP_NAME "root"
+#define DEF_MD5_HASH "ce6bb0ddf75be26c928ce2722e5f1625"
+#define DEF_SHA1_HASH "53bf474924c7876e2272db4a62fc64c8e2c18b51"
+#define DEF_SHA256_HASH "c2de156835127560dc1e8139846da7b7d002ac1b72024f6efb345cf27009c54c"
 
 #define loop_path(x) (x[0] == '.' && (x[1] == '\0' || (x[1] == '.' && x[2] == '\0')))
 
@@ -25,47 +32,46 @@ void announce_function(char *function) {
 }
 
 
-static fim_registry_value_data *fill_registry_value_struct(char * name, unsigned int type, unsigned int size,
-                                                           os_md5 hash_md5, os_sha1 hash_sha1, os_sha256 hash_sha256,
-                                                           unsigned int mtime, time_t last_event, unsigned int scanned,
-                                                           os_sha1 checksum, fim_event_mode mode) {
+static fim_registry_value_data *fill_registry_value_struct(unsigned int id, char *name, unsigned int type,
+                                                           unsigned int size, os_md5 hash_md5, os_sha1 hash_sha1,
+                                                           os_sha256 hash_sha256, unsigned int scanned,
+                                                           time_t last_event, os_sha1 checksum, fim_event_mode mode) {
 
     fim_registry_value_data *data = calloc(1, sizeof(fim_registry_value_data));
 
+    data->id = id;
     data->name = strdup(name);
     data->type = type;
     data->size = size;
-    // data->hash_md5 = hash_md5;
     snprintf(data->hash_md5, 33, "%s", hash_md5);
-    // data->hash_sha1 = hash_sha1;
     snprintf(data->hash_sha1, 65, "%s", hash_sha1);
-    // data->hash_sha256 = hash_sha256;
     snprintf(data->hash_sha256, 65, "%s", hash_sha256);
-
-    data->last_event = last_event;
     data->scanned = scanned;
-    // data->checksum = checksum;
+    data->last_event = last_event;
     snprintf(data->checksum, 65, "%s", checksum);
     data->mode = mode;
 
     return data;
 }
 
-static fim_registry_key *fill_registry_key_struct(char * path, unsigned int id, char * perm, char * uid, char * gid,
-                                                  char * user_name, char * group_name, unsigned int scanned,
-                                                  os_sha1 checksum) {
+
+
+static fim_registry_key *fill_registry_key_struct(unsigned int id, char *path, char *perm, char *uid, char *gid,
+                                                  char *user_name, char *group_name, unsigned int mtime, int arch,
+                                                  unsigned int scanned, os_sha1 checksum) {
 
     fim_registry_key *data = calloc(1, sizeof(fim_registry_key));
 
-    data->path = strdup(path);
     data->id = id;
+    data->path = strdup(path);
     data->perm = strdup(perm);
     data->uid = strdup(uid);
     data->gid = strdup(gid);
     data->user_name = strdup(user_name);
     data->group_name = strdup(group_name);
+    data->mtime = mtime;
+    data->arch = arch;
     data->scanned = scanned;
-    // data->checksum = checksum;
     snprintf(data->checksum, 65, "%s", checksum);
 
     return data;
@@ -78,13 +84,15 @@ void print_fim_registry_key_data(fim_entry *entry) {
 
     printf("\n~~~ Registry key ~~~\n");
     printf("\n---------------------------------\n");
-    printf("Path: %s\n", entry->registry_entry.key->path);
     printf("ID: %d\n", entry->registry_entry.key->id);
+    printf("Path: %s\n", entry->registry_entry.key->path);
     printf("Perm: %s\n", entry->registry_entry.key->perm);
     printf("UID: %s\n", entry->registry_entry.key->uid);
     printf("GID: %s\n", entry->registry_entry.key->gid);
     printf("User name: %s\n", entry->registry_entry.key->user_name);
     printf("Group name: %s\n", entry->registry_entry.key->group_name);
+    print("Modification time: %d\n", entry->registry_entry.key->mtime);
+    printf("Architecture: %d\n", entry->registry_entry.key->arch);
     printf("Scanned: %d\n", entry->registry_entry.key->scanned);
     printf("Checksum: %s\n", entry->registry_entry.key->checksum);
     printf("---------------------------------\n");
@@ -94,32 +102,34 @@ void print_fim_registry_value_data(fim_entry *entry) {
     if (entry->registry_entry.value == NULL) {
         return ;
     }
+
     printf("\n~~~ Registry value ~~~\n");
     printf("\n---------------------------------\n");
+    printf("ID: %d\n", entry->registry_entry.value->id);
     printf("Name: %s\n", entry->registry_entry.value->name);
     printf("Type: %d\n", entry->registry_entry.value->type);
     printf("Size: %d\n", entry->registry_entry.value->size);
     printf("Hash MD5: %s\n", entry->registry_entry.value->hash_md5);
     printf("Hash SHA1: %s\n", entry->registry_entry.value->hash_sha1);
     printf("Hash SHA256: %s\n", entry->registry_entry.value->hash_sha256);
-    printf("Last event: %lu\n", entry->registry_entry.value->last_event);
     printf("Scanned: %d\n", entry->registry_entry.value->scanned);
+    printf("Last event: %lu\n", entry->registry_entry.value->last_event);
     printf("Checksum: %s\n", entry->registry_entry.value->checksum);
     printf("Mode: %i\n", entry->registry_entry.value->mode);
     printf("---------------------------------\n");
 }
 
 /* Callback para printar entry */
-void print_entry(__attribute__((unused))fdb_t *fim_sql, fim_entry *entry,
-    __attribute__((unused))pthread_mutex_t *mutex, __attribute__((unused))void *alert,
-    __attribute__((unused))void *mode, __attribute__((unused))void *w_event) {
+void print_entry(__attribute__((unused))fdb_t *fim_sql,
+                 fim_entry *entry,
+                 __attribute__((unused))pthread_mutex_t *mutex,
+                 __attribute__((unused))void *alert,
+                 __attribute__((unused))void *mode,
+                 __attribute__((unused))void *w_event) {
 
     print_fim_registry_key_data(entry);
     print_fim_registry_value_data(entry);
 }
-
-#define DEF_REG_NAME "reg_name_"
-#define DEF_REG_PATH "HKEY_LOCAL_MACHINE\\TEST"
 
 int fill_entries_random(fdb_t *fim_sql, unsigned int num_keys, unsigned int num_entries) {
     unsigned int i, j;
@@ -129,20 +139,18 @@ int fill_entries_random(fdb_t *fim_sql, unsigned int num_keys, unsigned int num_
         char *reg_path = calloc(512, sizeof(char));
         snprintf(reg_path, 512, "%s_%i\\", DEF_REG_PATH, i);
 
-        fim_registry_key *key = fill_registry_key_struct(reg_path, 0, "rwxrwxrwx", "0", "0", "root", "root", rand() % 2,
-                                                     "53bf474924c7876e2272db4a62fc64c8e2c18b51");
+        fim_registry_key *key = fill_registry_key_struct(i, reg_path, DEF_PERM, DEF_UID, DEF_GID, DEF_USER_NAME,
+                                                         DEF_GROUP_NAME, rand() % 1500000000, rand() % 2, 1,
+                                                         DEF_SHA1_HASH);
 
         for (j = 0; j < num_entries; j++) {
             char *reg_name = calloc(512, sizeof(char));
             snprintf(reg_name, 512, "%s%i", DEF_REG_NAME, j);
 
-            fim_registry_value_data *value = fill_registry_value_struct(reg_name, rand() % 15, rand() % 256,
-                                                                    "ce6bb0ddf75be26c928ce2722e5f1625",
-                                                                    "53bf474924c7876e2272db4a62fc64c8e2c18b51",
-                                                                    "c2de156835127560dc1e8139846da7b7d002ac1b72024f6efb345cf27009c54c",
-                                                                    rand() % 1500000000, rand() % 1500000000, rand() % 2,
-                                                                    "53bf474924c7876e2272db4a62fc64c8e2c18b51",
-                                                                    FIM_SCHEDULED);
+            fim_registry_value_data *value = fill_registry_value_struct(i, reg_name, rand() % 11, rand() % 256,
+                                                                        DEF_MD5_HASH, DEF_SHA1_HASH, DEF_SHA256_HASH, 1,
+                                                                        rand() % 1500000000, DEF_SHA1_HASH,
+                                                                        FIM_SCHEDULED);
 
             entry = calloc(1, sizeof(fim_entry));
 
@@ -152,18 +160,18 @@ int fill_entries_random(fdb_t *fim_sql, unsigned int num_keys, unsigned int num_
 
             if (fim_db_insert_registry(fim_sql, entry)) {
                 printf("Error in fim_db_insert_registry() function: %s\n", reg_name);
-                //print_fim_file_data_full(data);
                 return FIMDB_ERR;
             }
-            fim_db_force_commit(fim_sql);
 
             free_registry_value(value);
             free(reg_name);
         }
+
         free(entry);
         free_registry_key(key);
         free(reg_path);
     }
+
     return 0;
 }
 
@@ -176,11 +184,8 @@ int main(int argc, char *argv[]) {
 
     nice(10);
 
-    // bool type = false;
     unsigned int num_keys = atoi(argv[1]);
     unsigned int num_values = atoi(argv[2]);
-    // int loop = atoi(argv[3]);
-    // char * file_test = argv[4];
 
     struct timespec start, end, commit;
 
